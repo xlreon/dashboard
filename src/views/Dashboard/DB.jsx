0d4da0ca2ecc6484947fc2178e47229e1ea6ffe1
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-07-30 02:00:18 +0530

    Real time data update added.

--- src/views/Dashboard/DB.jsx
+++ src/views/Dashboard/DB.jsx
@@ -7,6 +7,8 @@ import dashboardStyle from "assets/jss/material-dashboard-react/layouts/dashboar
 import CustomDrawer from "components/CustomDrawer/CustomDrawer.jsx";
 import features from './features.jsx';
 import CustomAppBar from 'components/CustomAppBar/CustomAppBar.jsx';
+import axios from 'axios';
+import md5 from 'md5';
 
 class DashBoard extends React.Component {
   
@@ -49,11 +51,38 @@ class DashBoard extends React.Component {
 
   componentDidMount() {
 
+    setInterval(() => this.recurGetInfo(),30000);
+
     var phones = JSON.parse(localStorage.getItem("phones"));
     var currentPhone = JSON.parse(localStorage.getItem("currPhone"));
     // console.log(currentPhone)
     this.setState({phones: phones, currentPhone : currentPhone});
+      setInterval(() => {
+        this.recurPhoneGet()
+      },30000)
+    
+  }
 
+  recurGetInfo = () => {
+    var body = { featureName : "info"};
+            
+            var formBody = [];
+            for (var property in body) {
+                var encodedKey = encodeURIComponent(property);
+                var encodedValue = encodeURIComponent(body[property]);
+                formBody.push(encodedKey + "=" + encodedValue);
+            }
+            formBody = formBody.join("&");
+            
+            axios.post(`http://localhost:8080/feature`, 
+                formBody
+            )
+            .then(res => { 
+              console.log('Get information notification sent.');
+            })
+            .catch(err => {
+              console.log('Get information notification failed to send')
+            })
   }
 
   handleChange = event => {
@@ -61,6 +90,70 @@ class DashBoard extends React.Component {
     console.log(event.target.value);
     this.setState({currentPhone: event.target.value !== undefined ? event.target.value : 0})
     console.log(this.state.currentPhone)
+  }
+
+  recurPhoneGet = () => {
+    const email = localStorage.getItem("email")
+        
+    var phoneList = [];
+        if (email)
+        {
+            var body = { email : email};
+            
+            var formBody = [];
+            for (var property in body) {
+                var encodedKey = encodeURIComponent(property);
+                var encodedValue = encodeURIComponent(body[property]);
+                formBody.push(encodedKey + "=" + encodedValue);
+            }
+            formBody = formBody.join("&");
+            
+            axios.post(`http://localhost:8080/imei/get`, 
+                formBody
+            )
+            .then(res => {
+            var imeiList = res.data.body.content;
+        
+            
+            // console.log(imeiList)
+            {imeiList.map((prop, key) => {
+            
+            var body = { imei: prop};
+            
+            var formBody = [];
+                for (var property in body) {
+                var encodedKey = encodeURIComponent(property);
+                var encodedValue = encodeURIComponent(body[property]);
+                formBody.push(encodedKey + "=" + encodedValue);
+                }
+                formBody = formBody.join("&");
+                
+                axios.post(`http://localhost:8080/phone/get`, 
+                formBody
+                )
+                .then(res => {
+                if (res.data.body.content !== null) {
+                    phoneList.push(res.data.body);
+                    // localStorage.setItem("phones",JSON.stringify(phoneList));
+                    localStorage.setItem('currHash',md5(JSON.stringify(res.data.body)))
+                    console.log(localStorage.getItem('currHash'))
+                    // console.log(this.state.phones)
+                    var prevHash = localStorage.getItem('prevHash');
+                    var currHash = localStorage.getItem('currHash')
+                    
+                    if (prevHash !== currHash) {
+                      localStorage.setItem("phones",JSON.stringify(phoneList));
+                      this.setState({phones: phoneList});
+                    }
+                  }
+                })
+                .catch(error => console.log(error))
+        
+            })}
+            
+            })
+            .catch(error => console.log(error))
+        }
   }
 
   
