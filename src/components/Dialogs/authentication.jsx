9d383675b6e5905bafb2110f524d0f2e07a26eac
Rohan Panda <rohan.panda1@gmail.com>
2018-08-02 16:21:28 +0530

    authentication ui

--- src/components/Dialogs/authentication.jsx
+++ src/components/Dialogs/authentication.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -8,17 +9,42 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
 import TextField from '@material-ui/core/TextField';
 import { Typography } from '../../../node_modules/@material-ui/core';
+import logo from "assets/img/pfa.png";
+import { withStyles } from '@material-ui/core/styles';
 
 function Transition(props) {
   return <Slide direction="up" {...props} />;
 }
+
+const styles = theme => ({
+    
+  img : {
+    marginBottom : 20,
+    marginTop : 20,
+  },
+  content : {
+    marginRight : 30,
+    marginLeft : 30
+  },
+  container : {
+    display: 'flex',
+    justifyContent : 'center',
+    alignItems : 'center',
+  },
+  textField : {
+    width : '100%'
+  },
+  dialog : {
+    borderRadius: '25px'
+  }
+});
 
 class AlertDialogSlide extends React.Component {
 
 
   render() {
 
-
+    const {classes} = this.props;
     return (
       <div>
         <Dialog
@@ -28,24 +54,28 @@ class AlertDialogSlide extends React.Component {
           onClose={this.props.onClose}
           aria-labelledby="alert-dialog-slide-title"
           aria-describedby="alert-dialog-slide-description"
+          className={classes.dialog}
         >
           {/* <DialogTitle id="alert-dialog-slide-title">
             {"Enter your password"}
           </DialogTitle> */}
-          <DialogContent>
-            <Typography variant='title'>Enter Password</Typography>
+          <DialogContent className={classes.content}>
+            <div className={classes.container}>
+                <img src={logo} alt="logo" className={classes.img} />
+            </div>
+            <Typography variant='subheading'>To continue, first verify it's you</Typography>
             <TextField
               id="password"
-              label="Password"
+              label="Enter your Password"
               type="password"
-              // className={classes.textField}
+              className={classes.textField}
               value={this.props.pass}
               onChange={this.props.handlePassChange}
               margin="normal"
             />
-            {this.props.invalid ? <Typography color='error'>invalid Password</Typography>:""}
+            {this.props.invalid ? <Typography color='error'>Invalid Password</Typography>:""}
           </DialogContent>
-          <DialogActions>
+          <DialogActions className={classes.container}>
             <Button onClick={this.props.handlePassSubmit} color="primary">
               Submit
             </Button>
@@ -56,4 +86,8 @@ class AlertDialogSlide extends React.Component {
   }
 }
 
-export default AlertDialogSlide;
+AlertDialogSlide.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AlertDialogSlide);
